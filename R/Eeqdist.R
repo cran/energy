28e3a4bf23d17960193b13cde5bddc122274df0e
Maria Rizzo <mrizzo @ bgnet.bgsu.edu>
2007-11-03 00:00:00 +0000

    version 1.0-8

--- R/Eeqdist.R
+++ R/Eeqdist.R
@@ -1,15 +1,14 @@
 ksample.e <- 
-function(x, sizes, distance = FALSE, ix = 1:sum(sizes), 
-         incomplete = FALSE, N = 100) {
+function(x, sizes, distance = FALSE, ix = 1:sum(sizes)) {
     ## computes the k-sample E-statistic for equal distributions
     ##   x:          pooled sample or distance matrix
     ##   sizes:      vector of sample sizes
     ##   distance:   TRUE if x is a distance matrix, otherwise FALSE
     ##   ix:         a permutation of row indices of x
-    ##   incomplete: if TRUE compute incomplete E-statistic
-    ##   N:          incomplete sample size
     ##   
-    ##   NOT much error checking here: for test use eqdist.etest
+    ## NOT much error checking here: for test use eqdist.etest
+    ## This version can be sourced R, but may be slower than the 
+    ## eqdist.e() version
     ## 
     k <- length(sizes)
     if (k == 1) return (0.0)
@@ -17,22 +16,20 @@ function(x, sizes, distance = FALSE, ix = 1:sum(sizes),
     e <- e0 <- 0
     if (!is.null(attr(x, "Size"))) distance <- TRUE
     x <- as.matrix(x)
-    if (incomplete == TRUE && distance == FALSE && any(sizes > N))
-        return(.incomplete.etest(x, sizes=sizes, R=0, N=N)$statistic)  
     
     if (distance == TRUE) {
         ##  same as test with 0 replicates
         b <- .C("ksampleEtest", 
-	    x = as.double(t(x)), 
-	    byrow = as.integer(1),
-	    nsamples = as.integer(length(sizes)), 
-	    sizes = as.integer(sizes),
-	    dim = as.integer(0), 
-	    R = as.integer(0), 
-	    e0 = as.double(e),
-	    e = as.double(e), 
-	    pval = as.double(e), 
-	    PACKAGE = "energy")           
+        x = as.double(t(x)), 
+        byrow = as.integer(1),
+        nsamples = as.integer(length(sizes)), 
+        sizes = as.integer(sizes),
+        dim = as.integer(0), 
+        R = as.integer(0), 
+        e0 = as.double(e),
+        e = as.double(e), 
+        pval = as.double(e), 
+        PACKAGE = "energy")           
         return (b$e0)
     }
 
@@ -59,27 +56,30 @@ function(x, sizes, distance = FALSE, ix = 1:sum(sizes),
     e
 }
 
+
+eqdist.e <- 
+function(x, sizes, distance = FALSE) {
+    ## multivariate E-statistic for testing equal distributions
+    ##   x:          matrix of pooled sample or distance matrix
+    ##   sizes:      vector of sample sizes
+    ##   distance:   logical, TRUE if x is a distance matrix, otherwise false
+    return(as.double(eqdist.etest(x, sizes, distance = FALSE, R = 0)$statistic))
+}    
+
 eqdist.etest <- 
-function(x, sizes, distance = FALSE, incomplete = FALSE, N = 100, R = 999) {
+function(x, sizes, distance = FALSE, R = 999) {
     ## multivariate E-test of the multisample hypothesis of equal distributions
     ##   x:          matrix of pooled sample or distance matrix
     ##   sizes:      vector of sample sizes
     ##   distance:   logical, TRUE if x is a distance matrix, otherwise false
     ##   R:          number of replicates
-    ##   incomplete: logical, TRUE if incomplete E statistics
-    ##   N:          sample size for incomplete version
     ##   
     
     nsamples <- length(sizes)
     if (nsamples < 2) return (NA)
     if (min(sizes) < 1) return (NA)
     if (!is.null(attr(x, "Size"))) distance <- TRUE
-    
-    if (nsamples == 2) {
-        if (incomplete == TRUE && distance == FALSE && any(sizes > N))
-            return(.incomplete.etest(x, sizes=sizes, N=N, R=R))  
-        }
-        
+           
     x <- as.matrix(x)
     if (nrow(x) != sum(sizes)) stop("nrow(x) should equal sum(sizes)")
     if (distance == FALSE && nrow(x) == ncol(x))
@@ -120,58 +120,3 @@ function(x, sizes, distance = FALSE, incomplete = FALSE, N = 100, R = 999) {
     e
 }
  
-
-.incomplete.etest <- 
-function(x, sizes, N = 100, R = 999) {
-    ##   intended to be called from eqdist.etest, not much error checking
-    ## 
-    ##   multivariate E-test of the multisample hypothesis of equal 
-    ##   distributions, incomplete E-statistic
-    ##   C library currently supports two sample test only
-    ##   x:          matrix of pooled sample or distance matrix
-    ##   sizes:      vector of sample sizes
-    ##   N:          max sample size for estimation of pairwise E
-    ##   R:          number of replicates
-    ##   
-    
-    k <- length(sizes)
-    if (k != 2) return (NA);
-    n <- cumsum(sizes) 
-    m <- 1 + c(0, n[1:(k-1)])
-    x <- as.matrix(x)
-    if (nrow(x) != sum(sizes)) return (NA)
-    d <- ncol(x)
-    r <- nrow(x)
-    str <- "Multivariate "
-    if (d == 1) str <- "Univariate "
-    e0 <- 0
-    pval <- 1
-    repl <- rep(0, R)  
-    b <- .C("twosampleIEtest", 
-        x = as.double(t(x)), 
-        byrow = as.integer(1),
-        sizes = as.integer(sizes),
-        dim = as.integer(d),
-        iN = as.integer(N),
-        R = as.integer(R), 
-        e0 = as.double(e0),
-        e = as.double(repl), 
-        pval = as.double(pval),
-        PACKAGE = "energy")           
-
-    methodname <- paste(str, length(sizes), 
-                    "-sample E-test of equal distributions", sep = "") 
-    sz <- paste(sizes, collapse = " ", sep = "")
-    dataname <- paste("sample sizes ", sz, ", replicates ", 
-                  R, ", N ", N, sep="")
-    names(b$e0) <- "(Incomplete) E-statistic"
-    e <- list(
-        method = methodname,
-        statistic = b$e0,
-        p.value = b$pval,
-        data.name = dataname)
-
-    class(e) <- "htest"        
-    e
-}
-
--- R/Emvnorm.R
+++ R/Emvnorm.R
@@ -1,15 +1,12 @@
 mvnorm.etest <- 
-function(x, mu = NULL, Sigma = NULL, R = 999) 
+function(x, R = 999) 
 {
     # parametric bootstrap E-test for multivariate normality
-    # matrix x is a multivariate sample with obs. in rows
-    # by default mu and Sigma will be estimated if not specified
     if (is.vector(x)) {
         n <- length(x)
         d <- 1
         bootobj <- boot(x, statistic = normal.e, R = R, sim = "parametric", 
-            ran.gen = function(x, y) {return(rnorm(n)) },
-            mu = mu, sigma = Sigma)
+            ran.gen = function(x, y) {return(rnorm(n)) })
         }
         else {
         n <- nrow(x)
@@ -19,32 +16,25 @@ function(x, mu = NULL, Sigma = NULL, R = 999)
                 return(matrix(rnorm(n * d), nrow = n, ncol = d)) })
         }
     p <- 1 - mean(bootobj$t < bootobj$t0)
-    method = "Energy test of multivariate normality"
     names(bootobj$t0) <- "E-statistic"
-    e <- list(call = match.call(),
-              statistic = bootobj$t0,
+    e <- list(statistic = bootobj$t0,
               p.value = p,
-              method = method,
+              method = "Energy test of multivariate normality: estimated parameters",
               data.name = paste("x, sample size ", n, ", dimension ", d, ", replicates ", R, sep = ""))
     class(e) <- "htest"        
     e                 
 }
 
 mvnorm.e <- 
-function(x, mu = NULL, Sigma = NULL) 
+function(x) 
 {
-    # E-statistic for multivariate normality    
-    n <- NROW(x)
-    d <- NCOL(x)
-    if (d < 2 || n < 2) return(normal.e(as.vector(x)))
-
-    x <- as.matrix(x)
-    if (is.null(mu))     
-        mu <- colMeans(x)
-    z <- sweep(x, 2, mu)    #subtract mean vector
-    if (is.null(Sigma)) 
-        Sigma <- var(x)
-    ev <- eigen(var(x), symmetric = TRUE)    #compute Sigma^(-1/2)
+    # E-statistic for multivariate normality
+    if (is.vector(x)) return(normal.e(x))
+    n <- nrow(x)
+    d <- ncol(x)
+    if (n < 2) return(normal.e(x))
+    z <- scale(x, scale = FALSE)    #subtract column means and 
+    ev <- eigen(var(x), symmetric = TRUE)    #compute S^(-1/2)
     P <- ev$vectors
     lambda <- ev$values    
     y <- z %*% (P %*% diag(1 / sqrt(lambda)) %*% t(P))
@@ -57,20 +47,13 @@ function(x, mu = NULL, Sigma = NULL)
 }
 
 normal.e <- 
-function(x, mu = NULL, sigma = NULL) 
+function(x) 
 {
-   # compute E for univariate normal GOF test
-   # univariate version is linearized for computational efficiency
-   if(is.matrix(x) && ncol(x) > 1)
-       stop("  normal.e() expects a univariate sample, 
-           but x is a multivariate sample; use mvnorm.e")
    x <- as.vector(x)
    y <- sort(x)
    n <- length(y)
    if (y[1] == y[n]) return (NA)
-   if (is.null(mu)) mu <- mean(y)
-   if (is.null(sigma)) sigma <- sd(y)
-   y <- (y - mu) / sigma
+   y <- scale(y) 
    K <- seq(1 - n, n - 1, 2)
    e <- 2 * (sum(2 * y * pnorm(y) + 2 * dnorm(y)) - n / sqrt(pi) - mean(K * y))
    e
